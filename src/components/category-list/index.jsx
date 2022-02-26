9339d93e1c90ea8563094dbc2ccf2c30f5bd0931
Mike Li <mike@dropoff.ai>
2022-02-26 00:09:16 -0500

    feat: adding categroy-list

--- src/components/category-list/index.jsx
+++ src/components/category-list/index.jsx
@@ -1,4 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Icon, NumberKeyboard, Calendar, ConfigProvider } from 'react-vant'
+import locale from './enUs'
+import style from './style.module.scss'
+
 export const CategoryList = (props) => {
-  return <div>CategoryList</div>
+  const { list, onClick, type } = props
+  const [visibleNum, setNumVisible] = useState(false)
+  const [visibleDate, setDateVisible] = useState(false)
+  const handleClick = (param) => {
+    if (type === 0) {
+      setNumVisible(true)
+    } else if (type === 1) {
+      onClick(param)
+    }
+  }
+  const onInput = (v) => {
+    console.log(v)
+    if (v === 'Date') {
+      setDateVisible(true)
+    }
+  }
+  const onDelete = () => {
+    console.log('Delete')
+  }
+
+  return (
+    <div className={style['list-wrapper']}>
+      {list.map((item, index) => {
+        return (
+          <div key={index} className={style['icon-block']}>
+            <div className={style.icon}>
+              <Icon name={item.name} onClick={() => handleClick(item.name)} />
+            </div>
+            <p>{item.desc}</p>
+          </div>
+        )
+      })}
+
+      <NumberKeyboard
+        theme="custom"
+        extraKey={['.', 'Date']}
+        closeButtonText="Enter"
+        visible={visibleNum}
+        onBlur={() => setNumVisible(false)}
+        onInput={(param) => onInput(param)}
+        onDelete={() => onDelete()}
+      />
+
+      <ConfigProvider locale={locale}>
+        <Calendar visible={visibleDate} onClose={() => setDateVisible(false)} />
+      </ConfigProvider>
+    </div>
+  )
 }
